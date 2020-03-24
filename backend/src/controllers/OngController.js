787f9bdd2c7285d75a3aac6c3100ee5c207d3446
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-24 16:55:58 -0300

    adiciona controller para encapsular as rotas

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,33 @@
+const connection = require('../database/connection')
+const crypto = require('crypto')
+
+module.exports = {
+
+    async index(req, res) {
+        const ongs = await connection('ongs').select('*')
+
+        return res.json(ongs)
+    },
+
+    async create(req, res) {
+        // desestruturando em variaveis a requisição
+        const { name, email, whatsapp, city, uf } = req.body
+
+        // cria um id randomico
+        const id = crypto.randomBytes(4).toString('HEX')
+
+        // insere os dados no banco
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        })
+
+        // retorna somente o id para o usuario
+        return res.json({ id })
+    }
+
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,39 +1,13 @@
 const express = require('express')
-const crypto = require('crypto')
-
-// conexao com o banco
-const connection = require('./database/connection')
+const OngController = require('./controllers/OngController')
 
 // instancia para usar as rotas
 const routes = express.Router()
 
 // rota para listas as Ongs
-routes.get('/ongs', async (req, res) => {
-    const ongs = await connection('ongs').select('*')
-
-    return res.json(ongs)
-})
+routes.get('/ongs', OngController.index)
 
 // rota para cadastrar uma nova Ong
-routes.post('/ongs', async (req, res) => {
-    // desestruturando em variaveis a requisição
-    const { name, email, whatsapp, city, uf } = req.body
-
-    // cria um id randomico
-    const id = crypto.randomBytes(4).toString('HEX')
-
-    // insere os dados no banco
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    })
-
-    // retorna somente o id para o usuario
-    return res.json({ id })
-})
+routes.post('/ongs', OngController.create)
 
 module.exports = routes;
