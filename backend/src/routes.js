a32ee923b2a28820ac5072c05681ddf5b5d96345
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-24 19:58:58 -0300

    dia24 - listar casos por Ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const OngController = require('./controllers/OngController')
 const IncidentsController = require('./controllers/IncidentsController')
+const ProfileController = require('./controllers/ProfileController')
 
 // instancia para usar as rotas
 const routes = express.Router()
@@ -10,11 +11,14 @@ routes.get('/ongs', OngController.index)
 // rota para cadastrar uma nova Ong
 routes.post('/ongs', OngController.create)
 
-// rota para listar os incidentes
+// rota para listar os casos por Ongs
+routes.get('/profile', ProfileController.index)
+
+// rota para listar os casos
 routes.get('/incidents', IncidentsController.index)
-// rota para cadastrar um novo incidente
+// rota para cadastrar um novo caso
 routes.post('/incidents', IncidentsController.create)
-// rota para deletar incidentes
+// rota para deletar caso
 routes.delete('/incidents/:id', IncidentsController.delete)
 
 module.exports = routes;
