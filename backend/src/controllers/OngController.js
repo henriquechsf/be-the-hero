0be68adf8879ad916fa4587d36e0078984e7aafc
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-24 17:50:30 -0300

    dia24 - cadastrar e listar incidentes

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -3,12 +3,14 @@ const crypto = require('crypto')
 
 module.exports = {
 
+    // listar Ongs
     async index(req, res) {
         const ongs = await connection('ongs').select('*')
 
         return res.json(ongs)
     },
 
+    // cadastro de Ongs
     async create(req, res) {
         // desestruturando em variaveis a requisição
         const { name, email, whatsapp, city, uf } = req.body
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const OngController = require('./controllers/OngController')
+const IncidentsController = require('./controllers/IncidentsController')
 
 // instancia para usar as rotas
 const routes = express.Router()
 
 // rota para listas as Ongs
 routes.get('/ongs', OngController.index)
-
 // rota para cadastrar uma nova Ong
 routes.post('/ongs', OngController.create)
+
+// rota para listar os incidentes
+routes.get('/incidents', IncidentsController.index)
+// rota para cadastrar um novo incidente
+routes.post('/incidents', IncidentsController.create)
 
 module.exports = routes;
