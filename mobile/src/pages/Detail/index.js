6ad34f7af6f3752c7efc9c22efc086f6bb6f2851
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-26 17:30:39 -0300

    dia26 - Envio de email e whats pela tela Detail

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
-import { View, Image, Text, TouchableOpacity } from 'react-native'
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native'
+import * as MailComposer from 'expo-mail-composer'
 
 import logoImg from '../../assets/logo.png'
 
@@ -11,8 +12,22 @@ function Detail() {
 
     const navigation = useNavigation()
 
+    const message = 'Olá APAE, estou entrando em contato, pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$ 120,00 '
+
     function navigateBack() {
         navigation.goBack()
+    }
+
+    function sendMail() {
+        MailComposer.composeAsync({
+            subject: 'Herói do caso: Cadelinha atropelada',
+            recipients: ['carloshenrique4m@gmail.com', 'ch.ferreir@hotmail.com'],
+            body: message,
+        })
+    }
+
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=55065993334111&text=${message}`)
     }
 
     return (
@@ -43,11 +58,11 @@ function Detail() {
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
 
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => { }}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                         <Text style={styles.actionText}>Whatsapp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.action} onPress={() => { }}>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
                         <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>
