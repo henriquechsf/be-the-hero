01b6d7cc1787ed29f9354cdeca41d362ebfa4208
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-25 22:59:47 -0300

    excluir casos

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -27,6 +27,23 @@ function Profile() {
         })
     }, [ongId])
 
+    // apagar caso cadastrado
+    async function handleDeleteIncident(id) {
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    authorization: ongId,
+                }
+            })
+
+            // atualiza o estado após o caso ser excluído
+            setIncidents(incidents.filter(incident => incident.id !== id))
+
+        } catch (err) {
+            alert('Erro ao deletar caso, tente novamente')
+        }
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -57,7 +74,7 @@ function Profile() {
                         <strong>VALOR:</strong>
                         <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-                        <button type="button">
+                        <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                             <FiTrash2 size={20} color="#a8a8b3" />
                         </button>
                     </li>
